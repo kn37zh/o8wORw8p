44f4469b05f6b86f63408e2d387d9746ee80c088
System Administrator <root@jaxmefs-MacBook-Air.local>
2017-10-18 00:58:02 +0300

    added range functionality (for lab #2)

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-void inputMenu(), mainMenu(), aboutMenu();
+void inputMenu(), mainMenu(), aboutMenu(), \
+	calculate(double a, double b, double step, double n), calculate(double x, double n);
 
 int main() {
 
@@ -33,38 +34,113 @@ void mainMenu() {
 	}
 	if (input!="input" && input!="help" && input!="exit") {
 		cout << "\033[H\033[2J" << std::flush;
-		cout << "\nThere is no \"" << input << "\" command.\nYou can only use \"input\", \"about\" and \"exit\"\n\n";
+		cout << "\nThere is no \"" << input << \
+			"\" command.\nYou can only use \"input\", \"about\" and \"exit\"\n\n";
 		mainMenu();
 	}
 }
 
 void inputMenu() {
+	string setOrRange;
 	cout << "\033[H\033[2J" << std::flush;
-	int x, n;
-	cout << "\nType x: ";
-	cin >> x;
-	cout << "Type n: ";
-	cin >> n;
+	cout << "Do you want to set x value or use range (type \"set\" or \"range\"): ";
+	cin >> setOrRange;
+	if (setOrRange=="set") {
+		cout << "\033[H\033[2J" << std::flush;
+
+		double x, n;
+		cout << "\nType x: ";
+		cin >> x;
+		cout << "Type n: ";
+		cin >> n;
+		calculate(x, n);
+	}
+	if (setOrRange=="range") {
+		cout << "\033[H\033[2J" << std::flush;
+
+		double a, b, step, n;
+		cout << "\nType a: ";
+		cin >> a;
+		cout << "Type b: ";
+		cin >> b;
+		cout << "Type step: ";
+		cin >> step;
+		cout << "Type n: ";
+		cin >> n;
+		cout << endl;
+		calculate(a, b, step, n);
+	}
+	if (setOrRange!="set" && setOrRange!="range") {
+		cout << "Error\n";
+		mainMenu();
+	}
+}
+
+void aboutMenu() {
+	cout << "\033[H\033[2J" << std::flush;
+	cout << "\n/–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––\\\n";
+	cout << "| Lab for Fundamentals of Software Engineering by Ihor Tytarenko v.1.0.0      |\n";
+	cout << "|                                                                             |\n";
+	cout << "| Email me if you have any ideas how to improve the app: jaxmef.uni@gmail.com |";
+	cout << "\n\\–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––/\n\n";
+	mainMenu();
+}
+
+void calculate(double a, double b, double step, double n) {
+	if (a>b or step<=0) {
+		cout << "Wrong input. Press any button to return to the main menu\n";
+		cin.get();
+		mainMenu();
+	}
+	double out;
+	string finalOut = "", fileSave;
+	for (double i=a; i<=b; i+=step) {
+		if (i < 0) {
+			out = 0;
+			for (int j=1; j<=n; j++) {
+				for (int k=1; k<=n; k++) {
+					out += (1/(i-j-k));
+				}
+			}
+		} else {
+			out = 1;
+			for (int j=0; j<=(n-3); j++) {
+				out *= (i-j);
+			}
+		}
+		cout << "x = " << i << "; y = " << out << ";\n";
+		finalOut += ("x = " + to_string(i) + "; y = " + to_string(out) + ";\n");
+	}
+	cout << "\nType \"yes\" to save the answer into a file: ";
+	cin >> fileSave;
+	if (fileSave == "yes") {
+		ofstream outFile;
+		outFile.open("a" + to_string(int(a)) + " b" + to_string(int(b)) + " step" + to_string(int(step)) \
+			+ " n" + to_string(int(n)) + " out.txt");
+		outFile << finalOut;
+		outFile.close();
+		cout << "File saved successfully (" << "a" + to_string(int(a)) + " b" + to_string(int(b)) \
+			+ " step" + to_string(int(step)) + " n" + to_string(int(n)) + " out.txt)";
+	}
+	cout << endl;
+	mainMenu();
+}
+
+void calculate(double x, double n) {
 	double out;
 	if (x < 0) {
-
 		out = 0;
-
-		for (double i=1; i<=n; i++) {
-			for (double j=1; j<=n; j++) {
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=n; j++) {
 				out += (1/(x-i-j));
 			}
 		}
 	} else {
-
 		out = 1;
-
-		for (double i=0; i<=(n-3); i++) {
+		for (int i=0; i<=(n-3); i++) {
 			out *= (x-i);
 		}
-
 	}
-
 	cout << "\nThe answer is " << out << "\n\nType \"yes\" to save the answer into a file: ";
 	string fileSave;
 	cin >> fileSave;
@@ -78,15 +154,3 @@ void inputMenu() {
 	}
 	mainMenu();
 }
-
-void aboutMenu() {
-	cout << "\033[H\033[2J" << std::flush;
-	cout << "\n/–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––\\\n";
-	cout << "| Lab for Fundamentals of Software Engineering by Ihor Tytarenko v.1.0.0      |\n";
-	cout << "|                                                                             |\n";
-	cout << "| Email me if you have any idea how to improve the app: jaxmef.uni@gmail.com  |";
-	cout << "\n\\–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––/\n\n";
-	mainMenu();
-}
-
-// min = a<b ? a:b or min = a>b ? b:a
